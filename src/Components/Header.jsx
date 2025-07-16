af59cd304f932c9e309f0a1b1c1831a817d25047
Orsouene <orsoueneelaouizeb@gmail.com>
2025-07-16 22:16:53 +0200

    Minor changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,9 +7,10 @@ function Header() {
      return (
           <div className='p-4 sticky top-0 z-50 bg-[#0F1923] h-18 text-xs sm:text-base'>
                <div className='flex justify-between items-center p-3 max-w-[1000px] m-auto bg-[#1F2937] rounded-md h-full shadow-2xl'>
-                    <div className='text-[#60A5FA] flex items-center gap-1 '>  
-                         <div className=' rounded-[50%] p-0.5 sm:p-1 font-bold text-[#0F1923] bg-[#60A5FA] border-2 border-[#0F1923] '>BS </div>
-                         <NavLink to={"/"} >BoolStation store </NavLink>    </div>
+                    <NavLink to={"/"}   className='text-[#60A5FA] flex items-center gap-1 '>  
+                         <div className=' rounded-[50%] p-0.5 sm:p-1 font-bold text-[#0F1923] bg-[#60A5FA] border-2 border-[#0F1923] '>  BS   </div>
+                       BoolStation store  
+                    </NavLink>
                     <div className='flex gap-x-1 items-center '>
                          <NavLink to={"/"}  className='text-white p-1 rounded-md text-sm font-light flex items-center  hover:bg-[#8E95A2]/30'> <CgGames className='mr-0.5' />  <span className='text-xs sm:text-base'>All Games </span>  </NavLink>
                          <NavLink to={`/Favourite`} className="bg-[#1F2937] text-white p-1 rounded-md text-sm  font-light flex items-center hover:bg-[#8E95A2]/30 " >
